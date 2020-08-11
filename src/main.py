58ffece32e99694485cbbe3b9a04dca69d196415
iproman <itechman@yandex.ru>
2020-08-11 09:38:12 +0500

    Parse data from html to write in csv.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 # Get html from url.
@@ -9,11 +10,47 @@ def get_html(url):
     print(r.status_code)
 
 
-# Main
+# Parse data from html to write in csv.
+def parse_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+
+    container = soup.find('body').find('div', class_='cmc-main-section__content')
+    tables = container.find_all('table')
+    trs = tables[2].find('tbody').find_all('tr', class_='cmc-table-row')
+
+    for tr in trs:
+        tds = tr.find_all('td')
+        try:
+            name = tds[1].find('a').text
+        except:
+            name = ''
+
+        try:
+            url = 'https://coinmarketcap.com' + tds[1].find('a').get('href')
+        except:
+            url = ''
+
+        try:
+            # price = tds[3].find('a').get('data-usd').strip()
+            price = tds[3].text
+        except:
+            price = ''
+
+        data = {
+            'name': name,
+            'url': url,
+            'price': price,
+        }
+
+
 def main():
     url = 'https://coinmarketcap.com/'
 
+    # Get html
     html = get_html(url)
+
+    # Write data to csv file
+    parse_data(html)
 
 
 if __name__ == '__main__':
